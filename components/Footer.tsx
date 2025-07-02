a57d1c34590961b12c504753606b01071db37368
davron77 <davronbekq467@gmail.com>
2025-07-02 23:33:43 +0500

    not yet, but good

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="w-full h-screen flex justify-center items-center">
+    <section className="w-full h-[200vh] flex justify-center items-center">
       <h1 className="text-4xl font-bold">Bye My King...</h1>
     </section>
   );
--- components/ImageAnimation.tsx
+++ components/ImageAnimation.tsx
@@ -12,7 +12,7 @@ const ImageAnimation = () => {
 
   useGSAP(
     () => {
-      // gsap.to(".box", {
+      // gsap.to(".img-1", {
       //   scrollTrigger: {
       //     trigger: ".box",
       //     start: "top bottom",
@@ -27,11 +27,55 @@ const ImageAnimation = () => {
         scrollTrigger: {
           trigger: ".img-holder",
           start: "top top",
-          end: "bottom top",
+          end: "200% top",
           markers: true,
           scrub: 1,
         },
         clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      });
+
+      gsap.to(".img-1", {
+        scrollTrigger: {
+          trigger: ".img-holder",
+          start: "top top",
+          end: "200% top",
+          markers: true,
+          scrub: 1,
+        },
+        x: -800,
+      });
+
+      gsap.to(".img-2", {
+        scrollTrigger: {
+          trigger: ".img-holder",
+          start: "top top",
+          end: "200% top",
+          markers: true,
+          scrub: 1,
+        },
+        x: -800,
+      });
+
+      gsap.to(".img-3", {
+        scrollTrigger: {
+          trigger: ".img-holder",
+          start: "top top",
+          end: "200% top",
+          markers: true,
+          scrub: 1,
+        },
+        x: 800,
+      });
+
+      gsap.to(".img-4", {
+        scrollTrigger: {
+          trigger: ".img-holder",
+          start: "top top",
+          end: "200% top",
+          markers: true,
+          scrub: 1,
+        },
+        x: 800,
       });
     },
     { scope: container }
@@ -39,14 +83,31 @@ const ImageAnimation = () => {
 
   return (
     <section className="relative" ref={container}>
-      {/* <div className="box"></div> */}
-      <div className="h-[200vh]">
-        <div className="img-holder">
-          <img
-            className="w-full h-screen object-cover"
-            src="/images/samarkhand.jpg"
-            alt="samarkhand"
-          />
+      <div className="h-[300vh]">
+        <div className="flex items-center justify-between sticky top-0">
+          <div className="flex gap-12">
+            <img className="img-1" src="/images/image-4.png" alt="" />
+            <div className="img-2 space-y-12">
+              <img src="/images/image-1.png" alt="" />
+              <img src="/images/image-2.png" alt="" />
+            </div>
+          </div>
+          <div>
+            <div className="img-holder">
+              <img
+                className="w-full h-screen object-cover"
+                src="/images/samarkhand.jpg"
+                alt="samarkhand"
+              />
+            </div>
+          </div>
+          <div className="flex gap-12">
+            <div className="img-3 space-y-12">
+              <img src="/images/image-1.png" alt="" />
+              <img src="/images/image-2.png" alt="" />
+            </div>
+            <img className="img-4" src="/images/image-4.png" alt="" />
+          </div>
         </div>
       </div>
     </section>
